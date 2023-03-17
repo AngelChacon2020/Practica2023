0a849c99627fb084cdb61a18db62c1a7116d45b9
Angel Chacon <angel>
2023-03-17 14:20:46 -0600

    Enrtorno de trabajo

---
+++ ControlActividades/src/main/java/com/example/ControlActividades/ControlActividadesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControlActividades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlActividadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlActividadesApplication.class, args);
+	}
+
+}
